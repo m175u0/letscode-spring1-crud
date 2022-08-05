b615d4954c5d3411d14d3b2718df2121957cfd22
Alberto <alberto@email.com>
2022-08-05 00:24:13 -0300

    Criação dos endpoints

--- src/main/java/br/com/bb/aula1/controller/ClienteController.java
+++ src/main/java/br/com/bb/aula1/controller/ClienteController.java
@@ -3,30 +3,66 @@ package br.com.bb.aula1.controller;
 import br.com.bb.aula1.model.Cliente;
 import br.com.bb.aula1.repository.IClienteData;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/client")
 public class ClienteController {
-    @Autowired
+//    @Autowired
     private IClienteData baseClientes;
 
-    @GetMapping("/client/list")
-    public ResponseEntity<List<Cliente>> listarTodosClientes() {
+    public ClienteController(IClienteData baseClientes) {
+        this.baseClientes = baseClientes;
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<Cliente>> listarClientes() {
         List<Cliente> clientes = new ArrayList<>();
         baseClientes.findAll().forEach(clientes::add);
         return new ResponseEntity<>(clientes, HttpStatus.OK);
     }
 
-    @PutMapping("/client/{id}")
-    public ResponseEntity<HttpStatus> criarNovoCliente() {
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente> atualizarCliente(@PathVariable("id") long id, @RequestBody Cliente cliente) {
+        Optional<Cliente> clienteData = baseClientes.findById(id);
 
+        if(clienteData.isPresent()) {
+            Cliente clienteAux = clienteData.get();
+            clienteAux.setVAT(cliente.getVAT());
+            clienteAux.setName(cliente.getName());
+            clienteAux.setAge(cliente.getAge());
+            clienteAux.setEmail(cliente.getEmail());
 
-        return new ResponseEntity<>(HttpStatus.OK);
+            return new ResponseEntity<>(baseClientes.save(clienteAux), HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
+    @PostMapping("/")
+    public ResponseEntity<Cliente> criarCliente(@RequestBody Cliente cliente) {
+        try {
+            Cliente clienteAux = baseClientes.save(new Cliente(cliente.getName(), cliente.getAge(), cliente.getVAT(), cliente.getEmail()));
+            return new ResponseEntity<>(clienteAux, HttpStatus.CREATED);
+        } catch (DataAccessException e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> apagarCliente(@PathVariable("id") long id) {
+        try {
+            baseClientes.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (DataAccessException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO CLIENTE (Id, Name, Age, VAT, Email) values (1, 'A', '10', 'AA111', 'a@a.com');
-INSERT INTO CLIENTE (Id, Name, Age, VAT, Email) values (2, 'B', '20', 'BB222', 'b@b.com');
-INSERT INTO CLIENTE (Id, Name, Age, VAT, Email) values (3, 'C', '30', 'CC333', 'c@c.com');
-INSERT INTO CLIENTE (Id, Name, Age, VAT, Email) values (4, 'D', '40', 'DD444', 'd@d.com');
-INSERT INTO CLIENTE (Id, Name, Age, VAT, Email) values (5, 'E', '50', 'EE555', 'e@e.com');
+INSERT INTO CLIENTE (Id, Name, Age, VAT, Email) values (999991, 'A', '10', 'AA111', 'a@a.com');
+INSERT INTO CLIENTE (Id, Name, Age, VAT, Email) values (999992, 'B', '20', 'BB222', 'b@b.com');
+INSERT INTO CLIENTE (Id, Name, Age, VAT, Email) values (999993, 'C', '30', 'CC333', 'c@c.com');
+INSERT INTO CLIENTE (Id, Name, Age, VAT, Email) values (999994, 'D', '40', 'DD444', 'd@d.com');
+INSERT INTO CLIENTE (Id, Name, Age, VAT, Email) values (999995, 'E', '50', 'EE555', 'e@e.com');
